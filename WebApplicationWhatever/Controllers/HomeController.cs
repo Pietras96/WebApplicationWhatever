341966403ad65bd45cd45cda58d710f233c1d9fa
Piotrek <Piotrek@DESKTOP-1D3S46N>
2021-10-22 19:56:28 +0200

    Zmienilem nazwe zmiennej

--- WebApplicationWhatever/Controllers/HomeController.cs
+++ WebApplicationWhatever/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace WebApplicationWhatever.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacyaa()
         {
             return View();
         }
